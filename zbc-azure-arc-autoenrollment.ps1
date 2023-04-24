3284361a5c1642d91a9e37fb126afaf5d5da2cb6
Tyler Lindberg <39677147+zveroboy152@users.noreply.github.com>
2023-04-23 23:29:25 -0700

    Update zbc-azure-arc-autoenrollment.ps1

--- zbc-azure-arc-autoenrollment.ps1
+++ zbc-azure-arc-autoenrollment.ps1
@@ -17,7 +17,7 @@ try {
     Invoke-WebRequest -UseBasicParsing -Uri "https://aka.ms/azcmagent-windows" -TimeoutSec 30 -OutFile "$env:TEMP\install_windows_azcmagent.ps1";
     & "$env:TEMP\install_windows_azcmagent.ps1";
     if ($LASTEXITCODE -ne 0) { exit 1; }
-    & "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect --service-principal-id "$servicePrincipalClientId" --service-principal-secret "$servicePrincipalSecret" --resource-group "$env:RESOURCE_GROUP" --tenant-id "$env:TENANT_ID" --location "$env:LOCATION" --subscription-id "$env:SUBSCRIPTION_ID" --cloud "$env:CLOUD" --tags "Datacenter=240,City=Roseville,StateOrDistrict=California,CountryOrRegion=USA,Environment=Homelab" --correlation-id "$env:CORRELATION_ID";
+    & "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect --service-principal-id "$servicePrincipalClientId" --service-principal-secret "$servicePrincipalSecret" --resource-group "$env:RESOURCE_GROUP" --tenant-id "$env:TENANT_ID" --location "$env:LOCATION" --subscription-id "$env:SUBSCRIPTION_ID" --cloud "$env:CLOUD" --tags "Datacenter=240,CountryOrRegion=USA,Environment=Homelab" --correlation-id "$env:CORRELATION_ID";
 }
 catch {
     $logBody = @{subscriptionId="$env:SUBSCRIPTION_ID";resourceGroup="$env:RESOURCE_GROUP";tenantId="$env:TENANT_ID";location="$env:LOCATION";correlationId="$env:CORRELATION_ID";authType="$env:AUTH_TYPE";operation="onboarding";messageType=$_.FullyQualifiedErrorId;message="$_";};
